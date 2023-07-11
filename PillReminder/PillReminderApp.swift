05f84c4dfc0776a8435a75597453438463db4f1d
Pac12Run8000 <gangstarr78992@>
2023-07-11 09:01:35 -0700

    Initial Commit

---
+++ PillReminder/PillReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PillReminderApp.swift
+//  PillReminder
+//
+//  Created by Michelle Grover on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PillReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
