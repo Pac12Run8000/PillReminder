dbc038e68c7815de45a22c1c7b6f6f140fca4566
Pac12Run8000 <gangstarr78992@>
2023-07-11 09:47:54 -0700

    Everything is working

--- PillReminder/Views/PerscriptionView.swift
+++ PillReminder/Views/PerscriptionView.swift
@@ -9,7 +9,50 @@ import SwiftUI
 
 struct PrescriptionView: View {
     var body: some View {
-        Text("Prescription View")
-            .font(.largeTitle)
+        VStack {
+                    Button(action: {
+                        print("Perscription Pill Reminder button tapped")
+                    }) {
+                        HStack {
+                            Image(systemName: "clock.fill")
+                            Text("Perscription Pill Reminder")
+                        }
+                    }
+                    .buttonStyle(CustomButtonStyle())
+                    
+                    Button(action: {
+                        print("Perscription History button tapped")
+                    }) {
+                        HStack {
+                            Image(systemName: "book.fill")
+                            Text("Perscription History")
+                        }
+                    }
+                    .buttonStyle(CustomButtonStyle())
+                    
+                    Button(action: {
+                        print("Submit Insurance Card button tapped")
+                    }) {
+                        HStack {
+                            Image(systemName: "creditcard.fill")
+                            Text("Submit Insurance Card")
+                        }
+                    }
+                    .buttonStyle(CustomButtonStyle())
+                    
+                    Button(action: {
+                        print("Pharmacy Chat button tapped")
+                    }) {
+                        HStack {
+                            Image(systemName: "message.fill")
+                            Text("Pharmacy Chat")
+                        }
+                    }
+                    .buttonStyle(CustomButtonStyle())
+                }
+                .padding()
+            
     }
 }
+
+
